fa5ccad2864782ca72c1084c92648914d18e1da1
chwong-cyberport <chunghangwong@cyberport.hk>
2025-04-17 11:53:46 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
 import "../app/globals.css"; // Ensure this path points to your global styles
-
-const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
+    <>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
